17e8e719d10cb72b41f71308f8788b57f1602366
Joanna-Lazarte <jlazartelagos@gmai.com>
2024-11-10 18:27:55 -0300

    llenadoDeClasesYaCreadas

--- TPO_ADO/src/entidades/Actor.java
+++ TPO_ADO/src/entidades/Actor.java
@@ -1,5 +1,40 @@
 package entidades;
 
 public class Actor {
-
+	private String nombre;
+	private int edad;
+	private String role;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public String getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(String edad) {
+		this.edad=edad;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		this.role=role;
+	}
+	
+	public Actor (String nombre, int edad, String role) {
+		this.nombre=nombre;
+		this.edad=edad;
+		this.role=role;
+		
+	}
+	
+	
 }
--- TPO_ADO/src/entidades/GrupoActores.java
+++ TPO_ADO/src/entidades/GrupoActores.java
@@ -1,5 +1,44 @@
 package entidades;
 
 public class GrupoActores {
+	private String nombreGrupo;
+	private List<Actor> actores = new ArrayList<>();
+	
+	public String getNombreGrupo() {
+		return nombreGrupo;
+	}
+	
+	public void setNombreGrupo(String nombreGrupo) {
+		this.nombreGrupo=nombreGrupo;
+	}
+	
+	public List<Actor> getActores() {
+		return actores;
+	}
+	
+	public void setActores(List<Actor> actores) {
+		this.actores=actores;
+	}
+	
+	
+	public GrupoActores (String nombreGrupo, List<Actor> actores) {
+		this.nombreGrupo=nombreGrupo;
+		this.actores=actores;
+		
+		
+	}
+	
+	public List<Actor> agregarActor(Actor actor){
+		
+		//
+		
+		//return actoresAgregados;
+	}
+	
+	public static void elimarActor(Actor ator) {
+		
+		//
+	}
+	
 
 }
